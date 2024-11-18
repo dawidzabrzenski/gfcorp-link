b705cf5af23da1b065d9b15e2089582ad316d17c
Dawid Zabrzeński <dawid.zabrzenski@gfcorp.pl>
2024-11-18 12:04:31 +0100

    Error rendering

--- src/ui/FormRow.jsx
+++ src/ui/FormRow.jsx
@@ -1,7 +1,14 @@
-function FormRow({ children }) {
+function FormRow({ children, error }) {
   return (
-    <div className="relative flex items-center justify-between gap-12">
-      {children}
+    <div className="flex-col items-center justify-between gap-4">
+      {error && (
+        <div className="inline-block -translate-y-2 whitespace-nowrap rounded-lg border border-red-500 bg-red-300 px-6 py-2 text-sm text-red-500">
+          {error}
+        </div>
+      )}
+      <div className="relative flex items-center justify-between gap-4">
+        {children}
+      </div>
     </div>
   );
 }
