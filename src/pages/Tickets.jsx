fa6c47d26e3dfee68a0af75406ccc7573f17a035
Dawid Zabrzeński <dawid.zabrzenski@gfcorp.pl>
2024-11-18 11:59:54 +0100

    Added loading skeleton

--- src/pages/Tickets.jsx
+++ src/pages/Tickets.jsx
@@ -7,10 +7,24 @@ import TableHeaderTitle from "../ui/TableHeaderTitle";
 import SearchBar from "../ui/SearchBar";
 import Spinner from "../ui/Spinner";
 
-function Tickets() {
-  const { isFetching, tickets } = useTickets();
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
-  if (isFetching) return <Spinner />;
+function Tickets() {
+  const { isFetching: isFetchingTickets, tickets } = useTickets();
+
+  // if (isFetching) return <Spinner />;
+
+  // if (isFetching)
+  //   return (
+  //     <div className="flex w-full items-center gap-6 px-12 py-4">
+  //       <SkeletonTheme baseColor="#dbdbdb" highlightColor="#e2e2e2">
+  //         <div>
+  //           <Skeleton count={12} style={{ width: "18rem", height: "1.5rem" }} />
+  //         </div>
+  //       </SkeletonTheme>
+  //     </div>
+  //   );
 
   return (
     <div className="flex flex-col gap-6 px-6">
@@ -31,10 +45,27 @@ function Tickets() {
           <TableHeaderTitle>Reporter</TableHeaderTitle>
           <TableHeaderTitle>Asignee</TableHeaderTitle>
         </Table.Header>
-        <Table.Body
-          data={tickets}
-          render={(ticket) => <Table.Row tickets={ticket} key={ticket.id} />}
-        />
+        {isFetchingTickets ? (
+          <div className="flex w-full items-center gap-6 px-12 py-4">
+            <SkeletonTheme baseColor="#dbdbdb" highlightColor="#ece6e6">
+              <div className="w-full">
+                <Skeleton
+                  count={12}
+                  style={{
+                    width: "100%",
+                    height: "2rem",
+                    marginBottom: "1rem",
+                  }}
+                />
+              </div>
+            </SkeletonTheme>
+          </div>
+        ) : (
+          <Table.Body
+            data={tickets}
+            render={(ticket) => <Table.Row tickets={ticket} key={ticket.id} />}
+          />
+        )}
       </Table>
     </div>
   );
