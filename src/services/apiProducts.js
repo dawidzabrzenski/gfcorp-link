89fa50455c1fa31a1fe57805f75fc3ae2f5eaf6b
dawidzabrzenski <dawidzabrzenski00@gmail.com>
2025-02-27 11:02:38 +0100

    feat: getAll query

--- src/services/apiProducts.js
+++ src/services/apiProducts.js
@@ -5,11 +5,8 @@ export async function getProducts() {
   const ERP_API_URL = import.meta.env.VITE_ERP_API_URL;
 
   try {
-    console.log(`http://${ERP_API_URL}/Product/GetBySku/UTT-31-032012-00`);
-    console.log(ERP_API_TOKEN);
-
     const res = await axios.get(
-      `http://${ERP_API_URL}/Product/GetBySku/UTT-31-032012-00`,
+      `http://${ERP_API_URL}/Product/GetAll?limit=50&page=1`,
       {
         headers: {
           Authorization: `Bearer ${ERP_API_TOKEN}`,
@@ -17,38 +14,38 @@ export async function getProducts() {
       },
     );
 
-    const filteredResults = [
-      {
-        twr_GIDNumer: res.data.twr_GIDNumer,
-        twr_Kod: res.data.twr_Kod,
-        twr_Nazwa: res.data.twr_Nazwa,
-        twr_Typ: res.data.twr_Typ,
-        twr_Nazwa1: res.data.twr_Nazwa1,
-        twr_Katalog: res.data.twr_Katalog,
-        twr_Ean: res.data.twr_Ean,
-        twr_Kategoria: res.data.twr_Kategoria,
-        twr_KategoriaId: res.data.twr_KategoriaId,
-        twr_kraj: res.data.twr_kraj,
-        twr_Sww: res.data.twr_Sww,
-        twr_Jm: res.data.twr_Jm,
-        twr_PCN: res.data.twr_PCN,
-        twr_grupaPod: res.data.twr_grupaPod,
-        twr_stawkaPod: res.data.twr_stawkaPod,
-        twr_GrupaPodSpr: res.data.twr_GrupaPodSpr,
-        twr_StawkaPodSpr: res.data.twr_StawkaPodSpr,
-        twr_Waga: res.data.twr_Waga,
-        twr_WJm: res.data.twr_WJm,
-        twr_WagaBrutto: res.data.twr_WagaBrutto,
-        twr_WJmBrutto: res.data.twr_WJmBrutto,
-        twr_ObjetoscL: res.data.twr_ObjetoscL,
-        twr_Wcenniku: res.data.twr_Wcenniku,
-        tap_Status: res.data.tap_Status,
-        tap_ZapowiedzDataOd: res.data.tap_ZapowiedzDataOd,
-        dataModyfikacji: res.data.dataModyfikacji,
-        producent: res.data.producent,
-        marka: res.data.marka,
-      },
-    ];
+    const filteredResults = await res.data.map((el) => ({
+      twr_GIDNumer: el.twr_GIDNumer,
+      twr_Kod: el.twr_Kod,
+      twr_Nazwa: el.twr_Nazwa,
+      twr_Typ: el.twr_Typ,
+      twr_Nazwa1: el.twr_Nazwa1,
+      twr_Katalog: el.twr_Katalog,
+      twr_Ean: el.twr_Ean,
+      twr_Kategoria: el.twr_Kategoria,
+      twr_KategoriaId: el.twr_KategoriaId,
+      twr_kraj: el.twr_kraj,
+      twr_Sww: el.twr_Sww,
+      twr_Jm: el.twr_Jm,
+      twr_PCN: el.twr_PCN,
+      twr_grupaPod: el.twr_grupaPod,
+      twr_stawkaPod: el.twr_stawkaPod,
+      twr_GrupaPodSpr: el.twr_GrupaPodSpr,
+      twr_StawkaPodSpr: el.twr_StawkaPodSpr,
+      twr_Waga: el.twr_Waga,
+      twr_WJm: el.twr_WJm,
+      twr_WagaBrutto: el.twr_WagaBrutto,
+      twr_WJmBrutto: el.twr_WJmBrutto,
+      twr_ObjetoscL: el.twr_ObjetoscL,
+      twr_Wcenniku: el.twr_Wcenniku,
+      tap_Status: el.tap_Status,
+      tap_ZapowiedzDataOd: el.tap_ZapowiedzDataOd,
+      dataModyfikacji: el.dataModyfikacji,
+      producent: el.producent,
+      marka: el.marka,
+    }));
+
+    console.log(filteredResults);
 
     return filteredResults;
   } catch (err) {
