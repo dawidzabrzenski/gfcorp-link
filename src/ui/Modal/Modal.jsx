c9992b615446937a0ca771b27497d4e9ccc8e31f
dawidzabrzenski <dawidzabrzenski00@gmail.com>
2025-03-07 15:12:50 +0100

    style: animate modal window

--- src/ui/Modal/Modal.jsx
+++ src/ui/Modal/Modal.jsx
@@ -6,12 +6,20 @@ const ModalContext = createContext();
 
 function Modal({ children }) {
   const [openName, setOpenName] = useState("");
+  const [isClosing, setIsClosing] = useState(false);
 
-  const close = () => setOpenName("");
+  const close = () => {
+    setIsClosing(true);
+    setTimeout(() => {
+      setOpenName("");
+      setIsClosing(false);
+    }, 150);
+  };
+
   const open = setOpenName;
 
   return (
-    <ModalContext.Provider value={{ openName, close, open }}>
+    <ModalContext.Provider value={{ openName, close, open, isClosing }}>
       {children}
     </ModalContext.Provider>
   );
@@ -24,22 +32,25 @@ function Open({ children, opens: opensWindowName }) {
 }
 
 function Window({ children, name }) {
-  const { openName, close } = useContext(ModalContext);
+  const { openName, close, isClosing } = useContext(ModalContext);
   const ref = useOutsideClick(close);
 
-  if (name !== openName) return null;
+  // Renderujemy modal, jeśli jest otwarty lub w trakcie zamykania
+  if (name !== openName && !isClosing) return null;
 
   return createPortal(
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 text-white backdrop-blur-sm">
       <div
         ref={ref}
-        className="relative transform-gpu rounded-lg bg-dark-lightbg p-8 shadow-lg transition-transform"
+        className={`relative rounded-lg bg-dark-lightbg p-8 shadow-lg transition-all duration-300 ease-in-out ${
+          isClosing ? "animate-modalDisappear" : "animate-modalAppear"
+        }`}
       >
         <button
           onClick={close}
           className="absolute right-4 top-3 rounded-md p-1.5 transition hover:text-red-500"
         >
-          &#10005;
+          ✕
         </button>
         <div>{cloneElement(children, { onCloseModal: close })}</div>
       </div>
